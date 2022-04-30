83547102ab09c03fc20c93a0017d0712deed4944
kenny1108Xiang <denny0979539212@gmail.com>
2022-04-30 13:02:07 +0800

    加入專案檔案。

---
+++ main.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+/*
+	這一檔案是在練習C語言
+*/
+int main(void) {
+	int sum, num1, num2;
+	num1 = 50;
+	num2 = 100;
+	sum = num1 + num2;
+	printf("%i + %i = %d",num1, num2, sum); //格式化輸出數字有 %i、%d
+	return 0;
+}
